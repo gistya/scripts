5109ce562c00554e1289686a252b790d51b5c579
Wiktor Obrębski <psychowico@gmail.com>
2024-09-03 19:18:09 +0200

    Polishing notes implementation

--- notes.lua
+++ notes.lua
@@ -15,8 +15,10 @@ NotesOverlay.ATTRS{
     viewscreens='dwarfmode',
     default_enabled=true,
     overlay_onupdate_max_freq_seconds=30,
-    fullscreen=true,
 }
+
+local waypoints = df.global.plotinfo.waypoints
+local map_notes = df.global.plotinfo.waypoints.points
 
 function NotesOverlay:init()
     self.notes = {}
@@ -35,11 +37,14 @@ end
 
 function NotesOverlay:onInput(keys)
     if keys._MOUSE_L then
-        local pos = dfhack.gui.getMousePos()
+        local top_most_screen = dfhack.gui.getDFViewscreen(true)
+        if dfhack.gui.matchFocusString('dwarfmode/Default', top_most_screen) then
+            local pos = dfhack.gui.getMousePos()
 
-        local note = self:clickedNote(pos)
-        if note ~= nil then
-            self:showNoteManager(note)
+            local note = self:clickedNote(pos)
+            if note ~= nil then
+                self:showNoteManager(note)
+            end
         end
     end
 end
@@ -52,11 +57,9 @@ function NotesOverlay:clickedNote(click_pos)
 
     self.last_click_pos = click_pos
 
-    local notes = df.global.plotinfo.waypoints.points
-
     local last_note_on_pos = nil
     local first_note_on_pos = nil
-    for _, note in pairs(notes) do
+    for _, note in ipairs(map_notes) do
         if same_xyz(note.pos, click_pos) then
             if last_note_on_pos == pos_curr_note then
                 return note
@@ -122,7 +125,7 @@ function NotesOverlay:reloadVisibleNotes()
         z=df.global.window_z
     }
 
-    for _, point in ipairs(df.global.plotinfo.waypoints.points) do
+    for _, point in ipairs(map_notes) do
         if viewport:isVisible(point.pos) then
             local pos = viewport:tileToScreen(point.pos)
             table.insert(self.notes, {
@@ -135,8 +138,7 @@ end
 
 NoteManager = defclass(NoteManager, gui.ZScreen)
 NoteManager.ATTRS{
-    focus_path='hotspot/menu',
-    hotspot_frame=DEFAULT_NIL,
+    focus_path='notes/note-manager',
     note=DEFAULT_NIL,
     on_update=DEFAULT_NIL,
 }
@@ -148,7 +150,6 @@ function NoteManager:init()
         widgets.Window{
             frame={w=35,h=20},
             frame_inset={t=1},
-            autoarrange_subviews=false,
             subviews={
                 widgets.HotkeyLabel {
                     key='CUSTOM_ALT_N',
@@ -158,50 +159,51 @@ function NoteManager:init()
                 },
                 text_editor.TextEditor{
                     view_id='name',
-                    focus_path='notes/name',
                     frame={t=1,h=3},
                     frame_style=gui.FRAME_INTERIOR,
-                    frame_style_b=nil,
                     init_text=self.note and self.note.name or ''
                 },
                 widgets.HotkeyLabel {
                     key='CUSTOM_ALT_C',
                     label='Comment',
-                    frame={t=4},
+                    frame={t=5},
                     on_activate=function() self.subviews.comment:setFocus(true) end,
                 },
                 text_editor.TextEditor{
                     view_id='comment',
-                    frame={t=5,b=3},
-                    focus_path='notes/comment',
+                    frame={t=6,b=3},
                     frame_style=gui.FRAME_INTERIOR,
                     init_text=self.note and self.note.comment or ''
                 },
                 widgets.Panel{
                     view_id='buttons',
-                    frame={b=0,h=3},
+                    frame={b=0,h=2},
                     autoarrange_subviews=true,
                     subviews={
-                        edit_mode and widgets.TextButton{
+                        widgets.HotkeyLabel{
                             view_id='Save',
                             frame={h=1},
                             label='Save',
                             key='CUSTOM_ALT_S',
+                            visible=edit_mode,
                             on_activate=function() self:saveNote() end,
                             enabled=function() return #self.subviews.name:getText() > 0 end,
-                        } or widgets.TextButton{
+                        },
+                        widgets.HotkeyLabel{
                             view_id='Create',
                             frame={h=1},
                             label='Create',
                             key='CUSTOM_ALT_S',
+                            visible=not edit_mode,
                             on_activate=function() self:createNote() end,
                             enabled=function() return #self.subviews.name:getText() > 0 end,
                         },
-                        edit_mode and widgets.TextButton{
+                        widgets.HotkeyLabel{
                             view_id='delete',
                             frame={h=1},
                             label='Delete',
                             key='CUSTOM_ALT_D',
+                            visible=edit_mode,
                             on_activate=function() self:deleteNote() end,
                         } or nil,
                     }
@@ -212,8 +214,8 @@ function NoteManager:init()
 end
 
 function NoteManager:createNote()
-    local x, y, z = pos2xyz(df.global.cursor)
-    if x == nil then
+    local cursor_pos = guidm.getCursorPos()
+    if cursor_pos == nil then
         print('Enable keyboard cursor to add a note.')
         return
     end
@@ -222,14 +224,12 @@ function NoteManager:createNote()
     local comment = self.subviews.comment:getText()
 
     if #name == 0 then
-        print('Note need at least a name')
+        dfhack.printerr('Note need at least a name')
         return
     end
 
-    local waypoints = df.global.plotinfo.waypoints
-    local notes = df.global.plotinfo.waypoints.points
 
-    notes:insert("#", {
+    map_notes:insert("#", {
         new=true,
 
         id = waypoints.next_point_id,
@@ -238,7 +238,7 @@ function NoteManager:createNote()
         bg_color=0,
         name=name,
         comment=comment,
-        pos=xyz2pos(x, y, z)
+        pos=cursor_pos
     })
     waypoints.next_point_id = waypoints.next_point_id + 1
 
@@ -258,11 +258,10 @@ function NoteManager:saveNote()
     local comment = self.subviews.comment:getText()
 
     if #name == 0 then
-        print('Note need at least a name')
+        dfhack.printerr('Note need at least a name')
         return
     end
 
-    local notes = df.global.plotinfo.waypoints.points
     self.note.name = name
     self.note.comment = comment
 
@@ -278,9 +277,9 @@ function NoteManager:deleteNote()
         return
     end
 
-    for ind, note in pairs(df.global.plotinfo.waypoints.points) do
-        if note == self.note then
-            df.global.plotinfo.waypoints.points:erase(ind)
+    for ind, note in pairs(map_notes) do
+        if note.id == self.note.id then
+            map_notes:erase(ind)
             break
         end
     end
@@ -307,9 +306,9 @@ local function main(args)
     end
 
     if args[1] == 'add' then
-        local x = pos2xyz(df.global.cursor)
-        if x == nil then
-            print('Enable keyboard cursor to add a note.')
+        local cursor_pos = guidm.getCursorPos()
+        if cursor_pos == nil then
+            dfhack.printerr('Enable keyboard cursor to add a note.')
             return
         end
 
